e12563b7ee6382dce0c0cdb1a8dfb9abc871c548
Mariano Eloy Fernández Osca <mariano.fernandez@gmail.com>
2011-10-27 20:55:26 +0200

    Eight marbles, all working fine, though it takes always 3 turns to find the heaviest one due to the use of binary chop algorithm.

---
+++ BalancePuzzle/test/org/katas/tdd/balance/v6/ScalesTest.java
@@ -0,0 +1,28 @@
+package org.katas.tdd.balance.v6;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ScalesTest {
+	
+	
+
+	@Test
+	public void weighNoMarble() {
+		Scales scales = new Scales();
+		int balance = scales.balance();
+		assertEquals(0, balance);
+		assertEquals(1, scales.getTurns());
+	}
+
+	@Test
+	public void weighOneMarble() {
+		Scales scales = new Scales();
+		scales.weighLeft(new Marble[] {new Marble(1)});
+		int balance = scales.balance();
+		assertEquals(-1, balance);
+		assertEquals(1, scales.getTurns());
+	}
+
+}
